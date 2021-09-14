3b298be092775c5ea6033dd551c36ebbf07344f6
WIN 10 <WIN 10@DESKTOP-PILGH1E>
2021-09-14 15:10:47 +0300

    Add project files.

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _logger; 
+
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger; 
+        }
+        // GET: api/<TestController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Accessed home controller");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<TestController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            _logger.LogDebug("Got a value");
+
+            return "value";
+        }
+
+        // POST api/<TestController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TestController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TestController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogWarn("Oops!");
+
+        }
+    }
+}
