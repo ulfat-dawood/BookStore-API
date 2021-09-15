e631bb540118526eab7c07acce7cfb8391b289da
Ulfat Dawood <ulfat_dawood@outlook.com>
2021-09-15 12:54:19 +0300

    First Commit

---
+++ BookStore-API/Mappings/Maps.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BookStore_API.Data;
+using BookStore_API.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            //Declare your mappings
+            //Mapping can go in either direction
+            CreateMap<Author, AuthorDTO>().ReverseMap();
+            CreateMap<Author, AuthorDTO>().ReverseMap();
+        }
+    }
+}
--- BookStore-API/Startup.cs
+++ BookStore-API/Startup.cs
@@ -1,5 +1,6 @@
 using BookStore_API.Contracts;
 using BookStore_API.Data;
+using BookStore_API.Mappings;
 using BookStore_API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -44,6 +45,9 @@ namespace BookStore_API
             });
 
             services.AddRazorPages();
+
+            services.AddAutoMapper(typeof(Maps));
+
             services.AddSwaggerGen(c => {
 
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo 
